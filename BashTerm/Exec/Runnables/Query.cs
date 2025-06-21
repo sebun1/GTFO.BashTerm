8b9be7392525cb4f2a9d13d9048e86fa2ee346cc
wyvern <nitrojector@takina.io>
2025-06-21 18:46:16 +0800

    fix: #12 feat: complete query runnable

--- BashTerm/Exec/Runnables/Query.cs
+++ BashTerm/Exec/Runnables/Query.cs
@@ -11,7 +11,29 @@ namespace BashTerm.Exec.Runnables;
 public class Query : IRunnable {
 	public string CommandName => "query";
 	public string Desc => "Queries the location of a single item (or multiple through piping)";
-	public string Man => "Usage";
+	public string Man => @"
+NAME
+		query - tool for querying the locations of items throughout the complex
+
+SYNOPSIS
+		QUERY <u>ITEM</u> -> ItemQueryResult / ItemQueryResults
+		ItemList -> QUERY [-S <u>SORT OPTIONS</u>]
+
+OPTIONS
+		-S, --SORT
+			Sort the list printed in the query summary in a specific order specified with a single sorting string, this also changes the order of items returned in ItemQueryResults.
+
+			There are three categories for sorting that can be configured, each specified with a flag
+				Item ID      I
+				Zone         Z or L
+				Capacity     C
+
+			To specify whether you want a category to be specified in ascending or descending order, immediately follow the flag with a + or - e.g. ""I-"", if no order is specified, the sort defaults to ascending order (i.e. +).
+
+			The priority for sorting is determined by the relative location of the flags in the string, if category x has its flag placed before category y, then query will try to sort via category x, if the results are inconclusive (i.e. there is a draw), it will fall back and compare category y, and so on.
+
+			For example, the sorting string ""Z+I+C-"" asks query to sort by zone number first in ascending order, if that fails sort by the item ID in ascending order, then sort capacity in descending order (items with most capacity comes first). Taking default behavior into mind, this sorting string can also be equivalently written as ""ZIC-"".
+";
 
 	public PipedPayload Run(string cmd, List<string> args, PipedPayload payload, LG_ComputerTerminal terminal) {
 		if (terminal == null) throw new NullTerminalInstanceException(CommandName);
@@ -24,13 +46,15 @@ public class Query : IRunnable {
 				List<ItemQueryResult> results = new List<ItemQueryResult>();
 				float timeCost = GetAdjustedQueryCost(items.Count);
 				string timeCostStr = timeCost.ToString("N0");
-				string priorityFlag = fp.GetVal("-s", "--sort") ?? "Z+I+Q-";
-				Logger.Debug($"Query cost: {timeCostStr}, priority flag: {priorityFlag}");
+				// TODO: Make default configurable in config
+				string sortFlag = (fp.GetVal("-s", "--sort") ?? "Z+I+C-").Trim().ToUpper();
+				Logger.Debug($"Query cost: {timeCostStr}, priority flag: {sortFlag}");
+
+				items.Sort(new TerminalItemComparator(sortFlag));
 
 				terminal.m_command.AddOutput(TerminalLineType.SpinningWaitDone,
 					$"Querying {items.Count} items (ETA: {timeCostStr}s)", timeCost);
-				PrintQuerySummary(items, terminal);
-				items.Sort(new TerminalItemComparator(priorityFlag)); // TODO: Fix sorting, not working
+				PrintQuerySummary(items, sortFlag, terminal);
 				foreach (var item in items) {
 					results.Add(new ItemQueryResult(
 						true,
@@ -78,11 +102,21 @@ public class Query : IRunnable {
 		}
 	}
 
-	private void PrintQuerySummary(List<iTerminalItem> items, LG_ComputerTerminal terminal) {
+	private void PrintQuerySummary(List<iTerminalItem> items, string sortFlag, LG_ComputerTerminal terminal) {
 		var lines = new Il2CppSystem.Collections.Generic.List<string>();
-		List<short> col = new List<short> { 0, 25, 50 };
-		terminal.m_command.AddOutput($"\n{Clr.Accent}<b>Query Summary</b>{Clr.End}", spacing: false);
-		lines.Add($"{Clr.Purple}{Fmt.Pos(col[0])}ID{Fmt.EndPos}{Fmt.Pos(col[1])}CAPACITY{Fmt.EndPos}{Fmt.Pos(col[2])}LOCATION{Fmt.EndPos}{Clr.End}\n");
+		List<short> col = new List<short> { 0, 25, 35 };
+		terminal.m_command.AddOutput($"\n{Clr.Accent}<b>Query Summary</b>{Clr.End} ", spacing: false);
+		terminal.m_command.AddOutput($"\n{Clr.Info}Sort=[{sortFlag}]{Clr.End}", spacing: false);
+
+		string resHeader = "";
+		// resHeader += $"{Fmt.Pos(col[0])}{Clr.Purple}ID{Clr.End} {Clr.Info}[I]{Clr.End}{Fmt.EndPos}";
+		// resHeader += $"{Fmt.Pos(col[1])}{Clr.Purple}CAPACITY{Clr.End} {Clr.Info}[C]{Clr.End}{Fmt.EndPos}";
+		// resHeader += $"{Fmt.Pos(col[2])}{Clr.Purple}LOCATION{Clr.End} {Clr.Info}[Z/L]{Clr.End}{Fmt.EndPos}";
+		resHeader += $"{Fmt.Pos(col[0])}ID {Clr.Info}[I]{Clr.End}{Fmt.EndPos}";
+		resHeader += $"{Fmt.Pos(col[1])}CAPACITY {Clr.Info}[C]{Clr.End}{Fmt.EndPos}";
+		resHeader += $"{Fmt.Pos(col[2])}LOCATION {Clr.Info}[Z/L]{Clr.End}{Fmt.EndPos}";
+		lines.Add($"{resHeader}\n");
+
 		foreach (var item in items) {
 			int cap = GetCapacity(item);
 			string capString = cap < 0 ? "-" : cap.ToString() + "%";
@@ -118,7 +152,7 @@ internal class TerminalItemComparator : IComparer<iTerminalItem> {
 
 			int result = field switch {
 				'I' => CompareID(x, y),
-				'Z' => CompareZone(x, y),
+				'Z' or 'L' => CompareZone(x, y),
 				'C' => CompareCapacity(x, y),
 				_ => 0
 			};
@@ -131,15 +165,15 @@ internal class TerminalItemComparator : IComparer<iTerminalItem> {
 		return 0;
 	}
 
-	public int CompareCapacity(iTerminalItem x, iTerminalItem y) { // flag C
+	public int CompareCapacity(iTerminalItem x, iTerminalItem y) {
 		return Query.GetCapacity(x).CompareTo(Query.GetCapacity(y));
 	}
 
-	public int CompareZone(iTerminalItem x, iTerminalItem y) { // flag Z
+	public int CompareZone(iTerminalItem x, iTerminalItem y) {
 		return x.SpawnNode.m_zone.ID.CompareTo(y.SpawnNode.m_zone.ID);
 	}
 
-	public int CompareID(iTerminalItem x, iTerminalItem y) { // flag I
-		return string.CompareOrdinal(x.TerminalItemKey, y.TerminalItemKey);
+	public int CompareID(iTerminalItem x, iTerminalItem y) {
+		return string.CompareOrdinal(Util.RemoveAllNumbers(x.TerminalItemKey), Util.RemoveAllNumbers(y.TerminalItemKey));
 	}
 }
